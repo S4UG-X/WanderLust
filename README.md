862776de3a5301c3b2ff7bf2759718abcfa23805
SaugatLamichhane <saugatlamichhane77@gmail.com>
2025-06-30 15:06:27 +0545

    Styling is done

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ routes
 2. Then, created listing.js model and give it a demo value to check if the database is working or not
 3. Creating init folder, where i could initialize my data. Store your 30 demo data here
 
+after completed doing work in sub branch, then you merge remotely from the website, and after that you have to merge locally by typing: git pull origin main.
 # Shortcut
 Ctrl + j ==> to toggle between file and terminal
 ctrl + shift + <> ==> to toggle between terminal
@@ -20,3 +21,10 @@ ctrl + shift + <> ==> to toggle between terminal
 # Branch Route
 created index route and show route
 created create route, update route and their corrosponding ejs files.
+
+# Branch styling
+ejs-mate npm package for boilerplate.ejs
+created /public/css/style.css
+using bootstrap for Navbar.
+linking cdn icons for icons
+styling index.ejs, new.ejs
--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
-const methodOverride = require("method-override")
-app.use(methodOverride("_method"))
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
@@ -14,7 +14,9 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 mongoose
   .connect("mongodb://localhost:27017/wanderlust")
   .then(() => {
@@ -25,37 +27,35 @@ mongoose
     console.log(err);
   });
 
-  //delete route
-  app.delete("/listings/:id",async (req, res)=>{
-    let {id}= req.params
-    await Listing.findByIdAndDelete(id)
-    res.redirect("/listings")
-
-  })
+//delete route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+});
 
 //Edit and Update
-app.get("/listings/:id/edit", async (req,res)=>{
-    let {id}= req.params
-    let listing= await Listing.findById(id)
-    res.render("edit.ejs", {listing})
-
-})
-app.patch("/listings/:id", async(req, res)=>{
-    let {id} = req.params
-    let newListing = req.body.listing
-    await Listing.findByIdAndUpdate(id, newListing)
-    // console.log(newListing)
-    res.redirect(`/listings/${id}`)
-})
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  res.render("edit.ejs", { listing, title: `Edit ${listing.title}` });
+});
+app.patch("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let newListing = req.body.listing;
+  await Listing.findByIdAndUpdate(id, newListing);
+  // console.log(newListing)
+  res.redirect(`/listings/${id}`);
+});
 
 //Add new route
 app.get("/listings/new", (req, res) => {
-  res.render("new.ejs");
+  res.render("new.ejs", {title: "New Listing"});
 });
 app.post("/listings", async (req, res) => {
   let data = req.body.listing;
   await Listing.create(data);
-  res.redirect("/listings")
+  res.redirect("/listings");
   // console.log(data)
 });
 
@@ -63,14 +63,14 @@ app.post("/listings", async (req, res) => {
 app.get("/listings/:id", async (req, res) => {
   let { id } = req.params;
   let listing = await Listing.findById(id);
-  res.render("show.ejs", { listing });
+  res.render("show.ejs", { listing, title: listing.title });
   // console.log(listing)
 });
 
 //index route
 app.get("/listings", async (req, res) => {
   let listings = await Listing.find({});
-  res.render("index.ejs", { listings });
+  res.render("index.ejs", { listings, title: "All Listings" });
   // console.log( typeof listings)
 });
 
